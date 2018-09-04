9df869390b7a4286ee389cdc857f0c9b00d1b24e
Reshma Reghu <41039813+resu15@users.noreply.github.com>
2018-09-04 17:26:44 +0500

    Add files via upload

---
+++ python9.py
@@ -0,0 +1,8 @@
+s=0
+N = int(input("Enter a number N: ")) 
+K = int(input("Enter a number K: "))
+for x in range(1,N+1):
+	print ("the value is:",x)
+	s=s+x
+for X in range(1,K+1):
+	print ("the value is :",s)
