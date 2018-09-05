ef7ac45d74ea835157b11fe5a5b7cd47e76c1504
Reshma Reghu <41039813+resu15@users.noreply.github.com>
2018-09-05 17:15:55 +0500

    Update python9.py

--- python9.py
+++ python9.py
@@ -2,7 +2,7 @@ s=0
 N = int(input("Enter a number N: ")) 
 K = int(input("Enter a number K: "))
 for x in range(1,N+1):
-	print ("the value is:",x)
+	print (x)
 for x in range(1,K+1):
 	s=s+x
 	print (s)
