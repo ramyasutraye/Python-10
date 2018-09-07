cd13ba168590b18461e82d3ea97216da836288cc
Reshma Reghu <41039813+resu15@users.noreply.github.com>
2018-09-07 16:46:39 +0530

    Update s24.py

--- s24.py
+++ s24.py
@@ -1,8 +1,8 @@
 l=int(input("Enter the lower limit for the range:"))
 u=int(input("Enter the upper limit for the range:"))
-for i in range(l,u+1):
-    if(i<=100000):
+if(l and u <=100000):
+    for i in range(l,u+1):
         if(i%2!=0):
             print(i)
-    else:
-        print "invalid"
+else:
+    print "invalid"
