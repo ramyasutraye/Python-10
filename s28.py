00031b526781a12ce154ff1d00cab57875a2e865
Reshma Reghu <41039813+resu15@users.noreply.github.com>
2018-09-08 15:41:12 +0530

    Update s28.py

--- s28.py
+++ s28.py
@@ -1,14 +1,18 @@
 l = int(input("Enter lower range: "))
 u = int(input("Enter upper range: "))
-for num in range(l,u+1):
-    n = len(str(num))
-    sum = 0
-    temp = num
-    while temp > 0:
-        digit = temp % 10
-        sum += digit ** n
-        temp //= 10
-    if num == sum:
-        print(num)
+if(l<=0):
+    print "invalid"
+if (u<=0):
+    print "invalid"
+    for num in range(l,u+1):
+        n = len(str(num))
+        sum = 0
+        temp = num
+        while temp > 0:
+            digit = temp % 10
+            sum += digit ** n
+            temp //= 10
+        if num == sum:
+            print(num)
    
     
