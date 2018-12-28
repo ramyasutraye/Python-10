a75ab060ac6da655d976729c935835c431e168c0
Reshma Reghu <41039813+resu15@users.noreply.github.com>
2018-12-28 15:02:48 +0530

    Update s6b5.py

--- s6b5.py
+++ s6b5.py
@@ -1,5 +1,12 @@
-n,m=input(" ").split(' '))
-if (int(n)*int(m))%2==0:
- print("even")
-else:
-  print("odd")
+def oddeven(a):
+	if a%2==0:
+		print("even");
+	else:
+		print("odd");
+def mul2():
+	try:
+		b=int(input())
+		a=int(input())
+		oddeven(b*a)
+	except:
+		print('invalid');
