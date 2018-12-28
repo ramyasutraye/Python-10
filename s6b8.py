2abb33a9eeb482982cce58297c2a048c904798fb
Reshma Reghu <41039813+resu15@users.noreply.github.com>
2018-12-28 15:34:58 +0530

    Create s6b8.py

---
+++ s6b8.py
@@ -0,0 +1,10 @@
+m=int(input("enter the no"))
+n=int(input("enter the no"))
+d=[]
+for i in range(0,m):
+    c=int(input("enter"))
+    d.append(c)
+if(n in d):
+    print("yes")
+else:
+    print("no")
