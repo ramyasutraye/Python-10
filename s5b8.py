3893ea202ae7a4d78d075211cb0b4535f82dbbce
Reshma Reghu <41039813+resu15@users.noreply.github.com>
2018-12-28 11:34:54 +0530

    Update s5b8.py

--- s5b8.py
+++ s5b8.py
@@ -1,6 +1,9 @@
 n=int(input(" "))
 c=0
-for i in range(0,n):
-        c=int(input(" "))
-        c=c+c
-print(c/n)
+if(n>0):
+        for i in range(0,n):
+                c=int(input(" "))
+                c=c+c
+        print(c/n)
+else:
+        print("invalid")
