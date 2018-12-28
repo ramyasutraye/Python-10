7c2985d77a7d4bc67ebc06914881206ac3a50861
Reshma Reghu <41039813+resu15@users.noreply.github.com>
2018-12-28 15:16:38 +0530

    Update s6b6.py

--- s6b6.py
+++ s6b6.py
@@ -1,16 +1,6 @@
-def main():
-   try:
-	a=0
-	b=0
-	n=input()
-for i in n:
-if i.isalpha():
-	a=a+1
-elif i.isnumeric():
-	b=b+1
-if a+b==len(n) and a>0 and b>0:
-	print('yes')
-else :
-	print('no')
-except:
-	print('invalid')
+n=input("enter")
+if(n.isprintable()):
+    if(n.isalpha()):
+       print("no")
+    else:
+      print("yes")
