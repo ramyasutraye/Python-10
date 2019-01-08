f2185a69afab5e5593516bd44f825eb6a71df23d
Reshma Reghu <41039813+resu15@users.noreply.github.com>
2019-01-08 11:42:02 +0530

    Update s7b9.py

--- s7b9.py
+++ s7b9.py
@@ -1,8 +1,11 @@
 n=int(input(" "))
 m=int(input(" "))
-print(n-m)
-x=n+m
-if(x%2==0):
-    print("even")
+t=n-m
+if t>=0:
+    x=n+m
+    if(x%2==0):
+        print("even")
+    else:
+        print("odd")
 else:
-    print("odd")
+    print("invalid")
